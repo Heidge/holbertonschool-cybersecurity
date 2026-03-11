12b86c62ff361e45cf0898afaef014ac1d2686e6
Heidge <axel.gore35@gmail.com>
2026-03-11 11:14:53 +0100

    feat: push flag

--- linux_security/1x00_linux_fundamentals/7-audit_gateway.sh
+++ linux_security/1x00_linux_fundamentals/7-audit_gateway.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "#!/bin/bash\ncat /var/www/html/secret_config.php" > /usr/local/bin/audit-read-secret && setfacl -m u:"$1":r /usr/local/bin/audit-read-secret && echo "$1 ALL=(root) NOPASSWD: /usr/local/bin/audit-read-secret" > /etc/sudoers.d/audit-gate
+echo -e "#!/bin/bash\ncat /var/www/html/secret_config.php" > /usr/local/bin/audit-read-secret && setfacl -m u:"$1":x /usr/local/bin/audit-read-secret && echo "$1 ALL=(root) NOPASSWD: /usr/local/bin/audit-read-secret" > /etc/sudoers.d/audit-gate
