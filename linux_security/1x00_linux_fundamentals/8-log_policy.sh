6b7c2bf6c12ca61f8d46a71a3dbd08488dba5b6f
Heidge <axel.gore35@gmail.com>
2026-03-11 16:23:08 +0100

    add: push scripta nd flag task 8

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chown root:$2 $1 && chmod 2750 "$1" && echo -e "$1*.log {\n\tcreate 0640 root "$2"\n}" > /etc/logrotate.d/app
+chown root:$2 $1 && chmod 2750 $1 && echo -e "$1*.log {\n\tcreate 0640 root $2\n}" > /etc/logrotate.d/app
