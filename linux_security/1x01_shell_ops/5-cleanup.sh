05cdfe80e194a42919fbba1895feb94997e87c44
Heidge <axel.gore35@gmail.com>
2026-03-12 15:39:22 +0100

    fix: replace passwd command by usermod

--- linux_security/1x01_shell_ops/5-cleanup.sh
+++ linux_security/1x01_shell_ops/5-cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while read -r user; do
     if id "$user" &>/dev/null; then
-        passwd -l "$user" &>/dev/null
+        usermod -L "$user" &>/dev/null
         echo "User $user locked"
     else
         echo "User $user not found"
