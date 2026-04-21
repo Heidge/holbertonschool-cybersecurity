8cb0cb07a5b7edfaff943bc3a3ebe9dfcaef120a
Heidge <axel.gore35@gmail.com>
2026-04-21 12:14:01 +0200

    add: script for check integrity config files and flag for task 2

--- linux_security/1x04_security_automation/sentinel.sh
+++ linux_security/1x04_security_automation/sentinel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-[[ -f sentinel.conf ]] && source sentinel.conf && [[ -n $SERVICES ]] || { echo "Config Error"; exit 1; }
+[[ -f sentinel.conf ]] && source sentinel.conf && [[ -n $SERVICES && -n $FILES_TO_WATCH ]] || { echo "Config Error"; exit 1; }
 
 check_services() {
     for svc in "${SERVICES[@]}"; do
-        if pgrep -f "$svc" > /dev/null; then
+        if pgrep -f "$svc"; then
             echo "OK: $svc is running"
         else
             if eval "$svc"; then
@@ -15,4 +15,20 @@ check_services() {
     done
 }
 
-check_services
+check_integrity() {
+    for file in "${FILES_TO_WATCH[@]}"; do
+        filename=$(basename "$file")
+        gold_file="/var/backups/sentinel/${filename}.gold"
+        current_hash=$(md5sum "$file" | cut -d ' ' -f1)
+        golden_hash=$(md5sum "$gold_file" | cut -d ' ' -f1)
+
+        if [[ "$current_hash" == "$golden_hash" ]]; then
+            echo "OK: $file integrity verified"
+        else
+            cp "$gold_file" "$file"
+            echo "FIXED: Restored $file"
+        fi
+    done
+}
+
+check_integrity 
